8a564074b161b1d3fd8721c95e23f52fec69d49a
Alessandro Pellizzi <alpelliz@e4r5p3.42roma.it>
2023-06-05 14:43:10 +0200

    prova

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: scastagn <scastagn@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 20:55:01 by scastagn          #+#    #+#             */
-/*   Updated: 2023/05/26 21:16:31 by scastagn         ###   ########.fr       */
+/*   Updated: 2023/06/05 14:42:30 by alpelliz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	main(int argc, char **argv, char **envp)
 {
     t_shell shell;
 
+
+    //int i = 0
     (void)argc;
     (void)argv;
     (void)envp;
